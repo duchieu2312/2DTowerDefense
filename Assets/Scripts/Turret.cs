4bdffc7c4aedfb4db546c3c69e6a9a29e53acb04
duchieu2312 <duchieu2312@gmail.com>
2025-03-25 23:54:50 +0700

    Implement turret firing bullets, enemy health

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,12 +5,16 @@ public class Turret : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float bulletsPerSecond = 1f;
 
     private Transform target;
+    private float timeUntilFire;
 
     private void Update()
     {
@@ -25,7 +29,23 @@ public class Turret : MonoBehaviour
         if (!CheckTargetIsInRange())
         {
             target = null;
+        } else
+        {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bulletsPerSecond)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
         }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
     }
 
     private void FindTarget()
